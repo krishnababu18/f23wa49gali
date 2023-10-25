51a0c0256a0b9873daf1a982ce22441f9d7ed31f
krishnababu18 <79692517+krishnababu18@users.noreply.github.com>
2023-10-25 18:45:03 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Krishna babu Gali' });
+  res.render('mydata', { title: 'Krishna babu Gali' });
 });
 
 module.exports = router;
